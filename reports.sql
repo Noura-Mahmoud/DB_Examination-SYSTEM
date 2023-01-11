dd23565dc41f5ac426d25b173c15ac543db51c84
raneem74 <raneemsaleh777@gmail.com>
2023-01-11 15:06:53 +0200

    added reports

--- reports.sql
+++ reports.sql
@@ -11,7 +11,7 @@ where s.dept_id = @deptID
 create proc student_grades @stdID int 
 with encryption 
 as 
-select s.first_name +' '+ s.last_name 'full name',c.name 'course',isnull(sc.grade,0) 
+select s.first_name +' '+ s.last_name 'full name',c.name 'course',isnull(sc.grade,0) 'grade'
 from student s 
 join Std_Crs sc
 on s.stud_id=sc.Std_Id and s.stud_id=@stdID
@@ -42,3 +42,31 @@ select c.name , t.topic_name
 from course c
 join topic t 
 on c.course_id = t.crs_id and c.course_id=@crsID
+
+--•	Report that takes exam number and returns the Questions in it and chocies
+
+create proc get_exam @examID int
+with encryption 
+as
+select q.q_text 'question' , c.choice_Id 'ID' , c.choice_text 'choice'
+from exam e
+join Exam_q eq
+on e.exam_id = eq.ex_Id and e.exam_id = @examID
+join question q
+on eq.q_Id = q.q_id
+join choice c 
+on q.q_id = c.question_Id
+order by q.q_id,c.choice_Id
+
+--•	Report that takes exam number and the student ID then returns the Questions in this exam with the student answers.
+create proc student_answers @examID int, @stdID int
+with encryption 
+as 
+select q.q_text 'question' , sa.ans_std 'ID' , c.choice_text 'answer'
+from std_ans sa
+join question q 
+on sa.question_id = q.q_id and sa.ex_id = @examID and sa.stud_Id = @stdID
+join choice c
+on q.q_id = c.question_Id and sa.ans_std = c.choice_Id
+order by q.q_id
+
